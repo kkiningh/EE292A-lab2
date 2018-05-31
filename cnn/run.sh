6701bf3bd06fa2ae575d7459a769f7581ef7d3e8
Kevin Kiningham <kkiningh@stanford.edu>
2018-05-31 11:43:15 -0700

    Add wrapper to run in emulation

---
+++ cnn/run.sh
@@ -0,0 +1,3 @@
+export CL_CONTEXT_EMULATOR_DEVICE_INTELFPGA=1
+cd ./bin
+./host --aocx=cnn_emu
--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
+export AOCL_BOARD_PACKAGE_ROOT="/usr/local/intelFPGA/17.1/hld/board/a10_ref"
 export INTELFPGAOCLSDKROOT="/usr/local/intelFPGA/17.1/hld/"
 export ALTERAOCLSDKROOT="/usr/local/intelFPGA/17.1/hld"
 export QSYS_ROOTDIR="/usr/local/intelFPGA/17.1/quartus/sopc_builder/bin"
-source /usr/local/intelFPGA/17.1/hld/init_opencl.sh
+source "${ALTERAOCLSDKROOT}"/init_opencl.sh
 
